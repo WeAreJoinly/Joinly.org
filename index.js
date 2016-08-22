ab6fe7b51a10134cc85f828140138e47d296788e
Timothy Borovkov <timofei.borovkov@icloud.com>
2016-08-22 20:03:43 +0300

    add queue management dashboard

--- index.js
+++ index.js
@@ -27,112 +27,144 @@ app.get('/apply', function(req, res) {
   res.redirect(301, 'https://docs.google.com/forms/d/e/1FAIpQLSc60UXvlEPpw-QmruLwiCTDZDsnKph1eYTh_D9P3TGDSLdbeQ/viewform');
 });
 
-app.get('/queue', function(req,res) {
-  res.send('<h1>This function is coming soon! :)</h1>');
+app.use("/manage", express.static(path.join(__dirname, 'manage')));
+
+app.get('/timePerCustomerInQueue', function(req, res){
+  var queueId = req.query.queue;
+  //TODO count estimated time per customer
+  var response = {
+    minutes: 0.5
+  }
+  res.send(response);
 });
 
-//Process socket.io
 io.on('connection', function (socket) {
-  console.log('user has been connected');
-  socket.on('leave', function(data){
-    //Someone wants to leave the queue
-    //data = {uid, queue}
+  socket.on('next', function(data){
+    //Queue manager clicked next customer button
+    //data = {queueId}
 
-    //1. Remove uid from queue in the database
-    database.ref('queue/'+data.queue+'/queue').on('value', function(snapshot) {
+    var queueId = data.queueId;
+
+    database.ref('queue/'+queueId).on('value', function(snapshot){
       var queue = snapshot.val();
-      var newQueue = {};
-      for (var userInQueue in queue) {
-        if (queue.hasOwnProperty(userInQueue)) {
-          if(userInQueue != data.uid){
-            if(queue[userInQueue] == 0){
-              //This is 0 user (it must stay)
-              newQueue[userInQueue] = 0;
-            }else{
-              if(queue[userInQueue] > queue[data.uid]){
-                newQueue[userInQueue] = queue[userInQueue] - 1;
-                //2. Inform user to move 1 place forward
 
-                //Notifications
-                if((queue[userInQueue] -1) == 2){
-                  //If next in queue
-                  //Send notification
-                  notify(userInQueue, "You are next in queue!");
-                }else if((queue[userInQueue] -1) == 1){
-                  //It is your time
-                  //Send notification
-                  notify(userInQueue, "Now it is your time!");
-                }else if((queue[userInQueue] -1) == 6){
-                  //There are now 10 people in front of you
-                  //Send notification
-                  notify(userInQueue, "There are still 5 people in front of you in the queue");
-                }else if((queue[userInQueue] -1) == 11){
-                  //There are now 10 people in front of you (come to buffer line)
-                  //Send notification
-                  notify(userInQueue, "It is time for you to get to the buffer line!");
-                }else if((queue[userInQueue] -1) == 21){
-                  //There are now 10 people in front of you
-                  //Send notification
-                  notify(userInQueue, "There are now 20 people in front of you in the queue");
-                }
-              }
-            }
+      //Generate new queue after user exit
+      var newQueue = {};
+      for (var userInQueue in queue.queue) {
+        if (queue.queue.hasOwnProperty(userInQueue)) {
+
+          var newPlaceInQueue = userInQueue - 1;
+
+          if(newPlaceInQueue == 0){
+            //You are now out of queue
+          }else if(newPlaceInQueue == 1){
+            //You are now first in queue
+            newQueue[userInQueue - 1] = queue.queue[userInQueue];
+            notify(userInQueue, 'This is your turn!');
+          }else if(newPlaceInQueue == 2){
+            //You are next in the queue
+            newQueue[userInQueue - 1] = queue.queue[userInQueue];
+            notify(userInQueue, 'You\'re next!');
+          }else if(newPlaceInQueue == 4){ //TODO What is the size of buffer queue?
+            //User should now join buffer queue
+            newQueue[userInQueue - 1] = queue.queue[userInQueue];
+            notify(userInQueue, 'It is time to go back to the queue!');
           }else{
-            //Do not add this to new queue, cause this is the user which wanted to leave
-            database.ref('user/'+userInQueue+'/ongoingQueue').set(null);
+            newQueue[userInQueue - 1] = queue.queue[userInQueue];
+          }
+
+          updateUser(queue.queue[userInQueue], 'place', userInQueue - 1);
+        }
+      }
+
+        database.ref('queue/'+queueId+'/queue').set(newQueue);
+
+    });
+
+  });
+
+  socket.on('exit', function(data){
+    //Someone wants to exit the queue
+    //data = {queueId, uid}
+    database.ref('queue/'+data.queueId).on('value', function(snapshot){
+      var queue = snapshot.val();
+
+      //Generate new queue after user exit
+      var newQueue = {};
+      for (var userInQueue in queue.queue) {
+        if (queue.queue.hasOwnProperty(userInQueue)) {
+          if(queue.queue[userInQueue] == data.uid){
+            //This is the user which wants to exit
+            //All users behind it must be moved one place forward
+                for (var placeInQueueOfUser in queue.queue) {
+                  if (queue.queue.hasOwnProperty(placeInQueueOfUser)) {
+                    //Check if user is in the queue after the leaving user
+                    if(placeInQueueOfUser > userInQueue){
+                      var newPlaceInQueue = placeInQueueOfUser - 1;
+                      newQueue[newPlaceInQueue] = queue.queue[placeInQueueOfUser];
+                      var uid = queue.queue[placeInQueueOfUser];
+                      //Send necessary notifications
+                        if(newPlaceInQueue == 1){
+                          //You are now first in queue
+                          notify(uid, 'This is your turn!');
+                        }else if(newPlaceInQueue == 2){
+                          //You are next in the queue
+                          notify(uid, 'You\'re next!');
+                        }else if(newPlaceInQueue == 4){ //TODO What is the size of buffer queue?
+                          //User should now join buffer queue
+                          notify(uid, 'It is time to go back to the queue!');
+                        }
+                        updateUser(uid, 'place', newPlaceInQueue);
+                    }else if (placeInQueueOfUser == userInQueue) {
+                      //Don't enter user to the newQueue, cause it is leaving now
+                      //Remove user's ongoing queue from database
+                      database.ref('user/'+uid+'/ongoingQueues').on('value', function(snapshot){
+                        var ongoingQueues = snapshot.val();
+                        for (var ongoingQueue in ongoingQueues) {
+                          if (ongoingQueues.hasOwnProperty(ongoingQueue)) {
+                            if(ongoingQueues[ongoingQueue] == data.queueId){
+                              //This is queue to remove
+                              database.ref('user/'+uid+'/ongoingQueues/'+data.queueId).set(null);
+                            }
+                          }
+                        }
+                      });
+                    }else{
+                      newQueue[placeInQueueOfUser] = queue.queue[placeInQueueOfUser];
+                    }
+                  }
+                }
           }
         }
       }
-      database.ref('queue/'+data.queue+'/queue').set(newQueue);
+
+      database.ref('queue/'+queueId+'/queue').set(newQueue);
     });
   });
 
-  socket.on('nextCustomer', function(data){
-    //Queue manager clicked 'Next customer' button
-    //data = {queue}
-    //1. Revove first user from the queue
-    database.ref('queue/'+data.queue+'/queue').on('value', function(snapshot) {
+  socket.on('join', function(data){
+    //Someone wants to join the queue
+    //data = {uid, queueId}
+
+    //Update user's ongoing queue
+    var newQueueKey = firebase.database().ref('user/' + data.uid + '/ongoingQueues').push().key;
+    database.ref('user/' + data.uid + '/ongoingQueues/' + newQueueKey).set(data.queueId);
+
+    //Insert user into queue.queue
+    database.ref('queue/' + data.queueId + '/queue').on('value', function(snapshot){
       var queue = snapshot.val();
-      var newQueue = {};
-      for (var userInQueue in queue) {
-        if (queue.hasOwnProperty(userInQueue)) {
-          if (queue[userInQueue] == 1) {
-            //This user must removed from the queue
-            socket.emit(data.queue, { uid: userInQueue, do: 'removeFromQueue'});
-            database.ref('user/'+userInQueue+'/ongoingQueue').set(null);
-          }else if (queue[userInQueue] == 0) {
-            //This is 0 user (it must stay)
-            newQueue[userInQueue] = 0;
-          }else{
-            newQueue[userInQueue] = queue[userInQueue] - 1;
-
-            if((queue[userInQueue] -1) == 2){
-              //If next in queue
-              //Send notification
-              notify(userInQueue, "You are next in queue!");
-            }else if((queue[userInQueue] -1) == 1){
-              //It is your time
-              //Send notification
-              notify(userInQueue, "Now it your time!");
-            }else if((queue[userInQueue] -1) == 11){
-              //There are now 10 people in front of you
-              //Send notification
-              notify(userInQueue, "There are now 10 people in front of you in the queue");
-            }else if((queue[userInQueue] -1) == 21){
-              //There are now 10 people in front of you
-              //Send notification
-              notify(userInQueue, "There are now 20 people in front of you in the queue");
-            }
-          }
-        }
-      } //--> For loop end
-      database.ref('queue/'+data.queue+'/queue').set(newQueue);
+      var placeInQueue = Object.keys(queue).length + 1;
+      database.ref('queue/' + data.queueId + '/queue/' + placeInQueue).set(queue.uid);
     });
   });
-});
+
+  function updateUser(uid, action, data){
+    socket.emit(uid, { action: action, data: data});
+  }
+})
 
 function notify(uid, text){
-  //TODO
+  //TODO Send push notification to user's phone
 }
 
 
